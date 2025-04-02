8b18f9bbe5cb0fdd3ee867f34b4a22242062f4c9
Abhishek kumar <abhi.arya97161@gmail.com>
2025-04-02 10:45:45 +0530

    Refactor App layout; remove unused CSS styles and add Navbar component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./Comp/Navbar/Navbar";
 import Upload from "./Pages/Upload";
 
 function App() {
-
   return (
     <>
+      <Navbar />
       <Upload />
     </>
   );
