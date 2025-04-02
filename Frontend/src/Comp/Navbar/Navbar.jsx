eed22c5c94c976b832e632dfe1bd033207572217
Abhishek kumar <abhi.arya97161@gmail.com>
2025-04-02 13:50:48 +0530

    Add video upload functionality with React Player; configure API URL and update package dependencies

--- Frontend/src/Comp/Navbar/Navbar.jsx
+++ Frontend/src/Comp/Navbar/Navbar.jsx
@@ -2,72 +2,52 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-end m-4 px-2 py-4">
-      <div className="flex w-[250px] items-center justify-around rounded-[10px] bg-[#f54990] shadow-[0_5px_15px_rgba(0,0,0,0.35),5px_10px_15px_rgba(245,73,144,0.5)]">
-        <button className="w-10 h-10 rounded-full flex items-center justify-center bg-transparent text-white transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-white/10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 1024 1024"
-            strokeWidth="0"
-            fill="currentColor"
-            stroke="currentColor"
-            className="text-xl"
-          >
-            <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>
-          </svg>
-        </button>
-        <button className="w-10 h-10 rounded-full flex items-center justify-center bg-transparent text-white transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-white/10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            aria-hidden="true"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            fill="none"
-            stroke="currentColor"
-            className="text-xl"
-          >
-            <path
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-            ></path>
-          </svg>
-        </button>
-        <button className="w-10 h-10 rounded-full flex items-center justify-center bg-transparent text-white transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-white/10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            strokeWidth="0"
-            fill="currentColor"
-            stroke="currentColor"
-            className="text-xl"
-          >
-            <path d="M12 2.5a5.5 5.5 0 0 1 3.096 10.047 9.005 9.005 0 0 1 5.9 8.181.75.75 0 1 1-1.499.044 7.5 7.5 0 0 0-14.993 0 .75.75 0 0 1-1.5-.045 9.005 9.005 0 0 1 5.9-8.18A5.5 5.5 0 0 1 12 2.5ZM8 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z"></path>
-          </svg>
-        </button>
-        <button className="w-10 h-10 rounded-full flex items-center justify-center bg-transparent text-white transition-all duration-300 ease-in-out hover:-translate-y-1 hover:bg-white/10">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            strokeLinejoin="round"
-            strokeLinecap="round"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            fill="none"
-            stroke="currentColor"
-            className="text-xl"
-          >
-            <circle r="1" cy="21" cx="9"></circle>
-            <circle r="1" cy="21" cx="20"></circle>
-            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-          </svg>
+    <div className="flex flex-col md:flex-row items-center md:items-start justify-center md:justify-between gap-6 p-4">
+      {/* Sign In Button */}
+      <div className="md:fixed md:top-4 md:right-4 z-50 flex items-center justify-center">
+        <button className="bg-[linear-gradient(#e9e9e9,#e9e9e9_50%,#fff)] group w-55 h-16 cursor-pointer inline-flex transition-all duration-300 overflow-visible p-1 rounded-full">
+          <div className="w-full h-full bg-[linear-gradient(to_top,#ececec,#fff)] overflow-hidden shadow-[0_0_1px_rgba(0,0,0,0.07),0_0_1px_rgba(0,0,0,0.05),0_3px_3px_rgba(0,0,0,0.25),0_1px_3px_rgba(0,0,0,0.12)] p-1 rounded-full hover:shadow-none duration-300">
+            <div className="w-full h-full text-xl gap-x-0.5 gap-y-0.5 justify-center text-[#101010] bg-[linear-gradient(#f4f4f4,#fefefe)] group-hover:bg-[linear-gradient(#e2e2e2,#fefefe)] duration-200 items-center text-[18px] font-medium gap-4 inline-flex overflow-hidden px-4 py-2 rounded-full group-hover:text-blue-600">
+              <svg
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                viewBox="0 0 64 64"
+                height="32px"
+                width="24px"
+              >
+                <g
+                  fill-rule="evenodd"
+                  fill="none"
+                  stroke-width="1"
+                  stroke="none"
+                >
+                  <g
+                    fill-rule="nonzero"
+                    transform="translate(3.000000, 2.000000)"
+                  >
+                    <path
+                      fill="#4285F4"
+                      d="M57.8123233,30.1515267 C57.8123233,27.7263183 57.6155321,25.9565533 57.1896408,24.1212666 L29.4960833,24.1212666 L29.4960833,35.0674653 L45.7515771,35.0674653 C45.4239683,37.7877475 43.6542033,41.8844383 39.7213169,44.6372555 L39.6661883,45.0037254 L48.4223791,51.7870338 L49.0290201,51.8475849 C54.6004021,46.7020943 57.8123233,39.1313952 57.8123233,30.1515267"
+                    ></path>
+                    <path
+                      fill="#34A853"
+                      d="M29.4960833,58.9921667 C37.4599129,58.9921667 44.1456164,56.3701671 49.0290201,51.8475849 L39.7213169,44.6372555 C37.2305867,46.3742596 33.887622,47.5868638 29.4960833,47.5868638 C21.6960582,47.5868638 15.0758763,42.4415991 12.7159637,35.3297782 L12.3700541,35.3591501 L3.26524241,42.4054492 L3.14617358,42.736447 C7.9965904,52.3717589 17.959737,58.9921667 29.4960833,58.9921667"
+                    ></path>
+                    <path
+                      fill="#FBBC05"
+                      d="M12.7159637,35.3297782 C12.0932812,33.4944915 11.7329116,31.5279353 11.7329116,29.4960833 C11.7329116,27.4640054 12.0932812,25.4976752 12.6832029,23.6623884 L12.6667095,23.2715173 L3.44779955,16.1120237 L3.14617358,16.2554937 C1.14708246,20.2539019 0,24.7439491 0,29.4960833 C0,34.2482175 1.14708246,38.7380388 3.14617358,42.736447 L12.7159637,35.3297782"
+                    ></path>
+                    <path
+                      fill="#EB4335"
+                      d="M29.4960833,11.4050769 C35.0347044,11.4050769 38.7707997,13.7975244 40.9011602,15.7968415 L49.2255853,7.66898166 C44.1130815,2.91684746 37.4599129,0 29.4960833,0 C17.959737,0 7.9965904,6.62018183 3.14617358,16.2554937 L12.6832029,23.6623884 C15.0758763,16.5505675 21.6960582,11.4050769 29.4960833,11.4050769"
+                    ></path>
+                  </g>
+                </g>
+              </svg>
+              <span className="ml-2 text-sm">Sign In with Google</span>
+            </div>
+          </div>
         </button>
       </div>
     </div>
--- Frontend/src/Pages/Upload.jsx
+++ Frontend/src/Pages/Upload.jsx
@@ -1,22 +1,98 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import ReactPlayer from "react-player";
 import style from "./Upload.module.css";
 
 const Upload = () => {
+  const [file, setFile] = useState();
+  const fileInputRef = useRef();
+
+  const videoURL = file ? URL.createObjectURL(file) : null;
+
+  const handleChangeVideo = () => {
+    fileInputRef.current.click(); // Trigger hidden input
+  };
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  console.log(file);
+
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-8rem)]">
-      <div className={style.container}>
-        <div className={style.folder}>
-          <div className={style.frontSide}>
-            <div className={style.tip}></div>
-            <div className={style.cover}></div>
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-8rem)] mx-4 gap-6">
+      {/* React Player */}
+      {videoURL && (
+        <div className="w-full max-w-[720px] flex justify-center items-center md:mt-20 mt-0">
+          <div className="rounded-2xl overflow-hidden bg-gray-600">
+            <ReactPlayer
+              url={videoURL}
+              controls
+              className="rounded-2xl"
+              style={{ maxHeight: "95vh", maxWidth: "100%" }}
+            />
           </div>
-          <div className={`${style.backSide} ${style.cover}`}></div>
         </div>
-        <label className={style.customFileUpload}>
-          <input className={style.title} type="file" />
-          Choose a file
-        </label>
-      </div>
+      )}
+
+      {/* Upload folder animation and input */}
+      {!file ? (
+        <div className={style.container}>
+          <div className={style.folder}>
+            <div className={style.frontSide}>
+              <div className={style.tip}></div>
+              <div className={style.cover}></div>
+            </div>
+            <div className={`${style.backSide} ${style.cover}`}></div>
+          </div>
+          <label className={style.customFileUpload}>
+            <input
+              className={style.title}
+              type="file"
+              accept="video/*"
+              onChange={handleFileChange}
+            />
+            Choose a file
+          </label>
+        </div>
+      ) : (
+        <>
+          <input
+            type="file"
+            accept="video/*"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            className="hidden"
+          />
+          <button
+            onClick={handleChangeVideo}
+            className="flex items-center justify-center cursor-pointer bg-gradient-to-r from-[#ffc75d] to-[#ffc708] shadow-[0_0_24px_#ffb20861] border-2 border-[#ffe825] rounded-full transition-all duration-300 px-5 py-2.5 text-[#09090b] font-bold text-shadow-custom hover:bg-[#ffc75d] hover:shadow-[0_0_34px_#ffb20861] hover:text-shadow-glow hover:border-[#ffe825]"
+          >
+            <svg
+              id="UploadToCloud"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              height="16px"
+              width="16px"
+              className="mr-2 filter drop-shadow-md"
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path
+                fill="#000000"
+                d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 
+               2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 
+               5-5 0-2.64-2.05-4.78-4.65-4.96zM14 
+               13v4h-4v-4H7l4.65-4.65c.2-.2.51-.2.71 0L17 13h-3z"
+              />
+            </svg>
+            Change Video
+          </button>
+        </>
+      )}
+
+      {/* File name */}
+      <p className="text-white max-w-[200px] truncate">
+        {file?.name || "Upload a video"}
+      </p>
     </div>
   );
 };
